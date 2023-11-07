97cae179752b64eeb192a4eb404060c9ce8b7054
Nicholas L <NLoo1@outlook.com>
2023-11-07 16:09:31 -0500

    Added Jasmine testing scripts

--- jasmine-testing-exercises-starter/calculator/calculator-test.js
+++ jasmine-testing-exercises-starter/calculator/calculator-test.js
@@ -1,7 +1,11 @@
 
-it('should calculate the monthly rate correctly', function () {
-  // ...
-});
+describe('correct monthly rates', function(){
+  it('should calculate the monthly rate correctly', function () {
+    // ...
+    expect(calculateMonthlyPayment(getCurrentUIValues())).toEqual("193.26");
+  });
+})
+
 
 
 it("should return a result with 2 decimal places", function() {
